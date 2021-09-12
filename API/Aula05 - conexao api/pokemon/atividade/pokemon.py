5e5f6052baf2ef51c5c097b0f0c364f024e6958a
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-09-12 02:07:21 -0300

    corrigindo arquivos

--- API/Aula05 - conexao api/pokemon/atividade/pokemon.py
+++ API/Aula05 - conexao api/pokemon/atividade/pokemon.py
@@ -3,12 +3,19 @@ from dataclasses import dataclass
 
 """
 Instruções para TODOS os exercícios/funções abaixo:
-1. Veja as instruções de como instalar o treinador e os testes no documento entregue junto com este arquivo.
-2. Se um determinado parâmetro de uma função deve ser inteiro, então esta função deve rejeitar valores não-numéricos ou numerais não-inteiros nesse parâmetro.
-3. Da mesma forma, se um parâmetro de uma função deve ser uma string, então esta função deve rejeitar valores que não sejam do tipo string nesse parâmetro.
+1. Veja as instruções de como instalar o treinador e os testes no documento
+entregue junto com este arquivo.
+2. Se um determinado parâmetro de uma função deve ser inteiro, então esta
+função deve rejeitar valores não-numéricos ou numerais não-inteiros nesse
+parâmetro.
+3. Da mesma forma, se um parâmetro de uma função deve ser uma string, então
+esta função deve rejeitar valores que não sejam do tipo string nesse parâmetro.
 4. Strings em branco são sempre consideradas inválidas.
-5. Todos os nomes de pokémons que aparecerem como parâmetros devem ser aceitos em minúsculas, MAIÚSCULAS ou até mesmo MiStUrAdO. Lembre-se dos métodos lower() e upper() da classe string.
-6. Todos os nomes de pokémons, cores, jogos, movimentos, etc. recebidos e devolvidos pela PokeAPI estão em letras minúsculas e assim devem ser mantidas.
+5. Todos os nomes de pokémons que aparecerem como parâmetros devem ser aceitos
+em minúsculas, MAIÚSCULAS ou até mesmo MiStUrAdO. Lembre-se dos métodos lower()
+e upper() da classe string.
+6. Todos os nomes de pokémons, cores, jogos, movimentos, etc. recebidos e
+devolvidos pela PokeAPI estão em letras minúsculas e assim devem ser mantidas.
 
 Alguns exemplos de URLs que podem servir de inspiração:
 http://pokeapi.co/api/v2/
@@ -25,7 +32,8 @@ http://pokeapi.co/api/v2/pokemon-color/2/
 """
 
 """
-Não altere estas URLs. Elas são utilizadas para conectar no treinador e no PokeAPI, respectivamente.
+Não altere estas URLs. Elas são utilizadas para conectar no treinador e no
+PokeAPI, respectivamente.
 """
 site_treinador = "http://127.0.0.1:9000"
 site_pokeapi = "https://pokeapi.co"
@@ -54,7 +62,8 @@ class PokemonJaCadastradoException(Exception):
 
 
 """
-Esta função certifica-se de que seu parâmetro é um número inteiro e lança uma ValueError se não for.
+Esta função certifica-se de que seu parâmetro é um número inteiro e lança uma
+ValueError se não for.
 """
 
 
@@ -64,7 +73,8 @@ def check_int(a):
 
 
 """
-Esta função certifica-se de que seu parâmetro é uma string e que não está vazia e lança uma ValueError se não for.
+Esta função certifica-se de que seu parâmetro é uma string e que não está vazia
+e lança uma ValueError se não for.
 """
 
 
@@ -135,7 +145,8 @@ def numero_do_pokemon(nome):
 
 
 """
-3. Dado o nome ou número de um pokémon, qual é o nome da cor (em inglês) predominante dele?
+3. Dado o nome ou número de um pokémon, qual é o nome da cor (em inglês)
+predominante dele?
 """
 
 
@@ -150,9 +161,12 @@ def color_of_pokemon(nome):
 
 
 """
-4. Dado o nome ou número de um pokémon, qual é o nome da cor (em português) predominante dele?
-Os nomes de cores possíveis em português são "marrom", "amarelo", "azul", "rosa", "cinza", "roxo", "vermelho", "branco", "verde" e "preto".
-No entanto, a pokeapi ainda não foi traduzida para o português! Como você pode dar um jeito nisso?
+4. Dado o nome ou número de um pokémon, qual é o nome da cor (em português)
+predominante dele?
+Os nomes de cores possíveis em português são "marrom", "amarelo", "azul",
+"rosa", "cinza", "roxo", "vermelho", "branco", "verde" e "preto".
+No entanto, a pokeapi ainda não foi traduzida para o português! Como você pode
+dar um jeito nisso?
 """
 
 
@@ -163,8 +177,11 @@ def cor_do_pokemon(nome):
 
 """
 5. Dado o nome de um pokémon, quais são os tipos no qual ele se enquadra?
-Os nomes dos tipos de pokémons em português são "normal", "lutador", "voador", "veneno", "terra", "pedra", "inseto", "fantasma", "aço", "fogo", "água", "grama", "elétrico", "psíquico", "gelo", "dragão", "noturno" e "fada".
-Todo pokémon pode pertencer a um ou a dois tipos diferentes. Retorne uma lista contendo os tipos, mesmo que haja somente um.
+Os nomes dos tipos de pokémons em português são "normal", "lutador", "voador",
+"veneno", "terra", "pedra", "inseto", "fantasma", "aço", "fogo", "água",
+"grama", "elétrico", "psíquico", "gelo", "dragão", "noturno" e "fada".
+Todo pokémon pode pertencer a um ou a dois tipos diferentes. Retorne uma lista
+contendo os tipos, mesmo que haja somente um.
 Se houver dois tipos, a ordem não é importante.
 """
 
@@ -208,7 +225,8 @@ tipos_do_pokemon('pikachu')
 """
 6. Dado o nome de um pokémon, liste de qual pokémon ele evoluiu.
 Por exemplo, evolucao_anterior('venusaur') == 'ivysaur'
-Retorne None se o pokémon não tem evolução anterior. Por exemplo, evolucao_anterior('bulbasaur') == None
+Retorne None se o pokémon não tem evolução anterior. Por exemplo,
+evolucao_anterior('bulbasaur') == None
 """
 
 
@@ -219,13 +237,19 @@ def evolucao_anterior(nome):
 """
 7. Dado o nome de um pokémon, liste para quais pokémons ele pode evoluiur.
 Por exemplo, evolucoes_proximas('ivysaur') == ['venusaur'].
-A maioria dos pokémons que podem evoluir, só podem evoluir para um único tipo de pokémon próximo. No entanto, há alguns que podem evoluir para dois ou mais tipos diferentes de pokémons.
-Se houver múltiplas possibilidades de evoluções, a ordem delas não importa. Por exemplo:
+A maioria dos pokémons que podem evoluir, só podem evoluir para um único tipo
+de pokémon próximo. No entanto, há alguns que podem evoluir para dois ou mais
+tipos diferentes de pokémons.
+Se houver múltiplas possibilidades de evoluções, a ordem delas não importa.
+Por exemplo:
 evolucoes_proximas('poliwhirl') == ['poliwrath', 'politoed']
-Note que esta função dá como resultado somente o próximo passo evolutivo. Assim sendo, evolucoes_proximas('poliwag') == ['poliwhirl']
-Se o pokémon não evolui, retorne uma lista vazia. Por exemplo, evolucoes_proximas('celebi') == []
+Note que esta função dá como resultado somente o próximo passo evolutivo.
+Assim sendo, evolucoes_proximas('poliwag') == ['poliwhirl']
+Se o pokémon não evolui, retorne uma lista vazia. Por exemplo,
+evolucoes_proximas('celebi') == []
 
-O exercicio 7 é opcional e bastante dificil. Se quiser, desligue os testes e vá para o 8!
+O exercicio 7 é opcional e bastante dificil. Se quiser, desligue os testes e
+vá para o 8!
 """
 
 
@@ -235,9 +259,12 @@ def evolucoes_proximas(nome):
 
 """
 8. A medida que ganham pontos de experiência, os pokémons sobem de nível.
-É possível determinar o nível (1 a 100) em que um pokémon se encontra com base na quantidade de pontos de experiência que ele tem.
-Entretanto, cada tipo de pokémon adota uma curva de level-up diferente (na verdade, existem apenas 6 curvas de level-up diferentes).
-Assim sendo, dado um nome de pokémon e uma quantidade de pontos de experiência, retorne o nível em que este pokémon está.
+É possível determinar o nível (1 a 100) em que um pokémon se encontra com base
+na quantidade de pontos de experiência que ele tem.
+Entretanto, cada tipo de pokémon adota uma curva de level-up diferente
+(na verdade, existem apenas 6 curvas de level-up diferentes).
+Assim sendo, dado um nome de pokémon e uma quantidade de pontos de experiência,
+retorne o nível em que este pokémon está.
 Valores negativos de experiência devem ser considerados inválidos.
 dica: na URL pokemon-species, procure growth rate
 """
@@ -248,20 +275,21 @@ def nivel_do_pokemon(nome, experiencia):
 
 
 """
-A partir daqui, você precisará rodar o servidor treinador.py na sua máquina para poder
-fazer a atividade. Não precisa mexer no arquivo, basta rodar ele.
+A partir daqui, você precisará rodar o servidor treinador.py na sua máquina
+para poder fazer a atividade. Não precisa mexer no arquivo, basta rodar ele.
 
-Os testes relativos ao treinador.py estao no arquivo pokemon_treinador_unittest.py
+Os testes relativos ao treinador.py estao no arquivo
+pokemon_treinador_unittest.py
 
 9. Dado um nome de treinador, cadastre-o na API de treinador.
-Retorne True se um treinador com esse nome foi criado e 
+Retorne True se um treinador com esse nome foi criado e
         False em caso contrário (já existia).
 
 Dicas teste 9: Use o verbo PUT, URL {site_treinador}/treinador/{nome}
 para criar um treinador. Se ele já existe, será retornado um cod de status
 303. Se não existe, cod status 202.
 
-dica: considere as linhas 
+dica: considere as linhas
       r = requests.put(url)
       status_code = r.status_code
 
@@ -274,26 +302,29 @@ def cadastrar_treinador(nome):
 
 
 """
-10. Imagine que você capturou dois pokémons do mesmo tipo. 
+10. Imagine que você capturou dois pokémons do mesmo tipo.
 Para diferenciá-los, você dá nomes diferentes (apelidos) para eles.
-Logo, um treinador pode ter mais do que um pokémon de um determinado tipo, 
+Logo, um treinador pode ter mais do que um pokémon de um determinado tipo,
 mas não pode ter dois pokémons diferentes com o mesmo apelido.
 
-Dados: um nome de treinador, um apelido de pokémon, um tipo de pokémon e uma quantidade de experiência, 
+Dados: um nome de treinador, um apelido de pokémon, um tipo de pokémon e uma
+quantidade de experiência,
 
-cadastre o pokémon com o tipo correspondente, na lista do treinador que foi passado,
-usando a API (o servidor) do treinador.
+cadastre o pokémon com o tipo correspondente, na lista do treinador que foi
+passado, usando a API (o servidor) do treinador.
 Certifique-se de que todos os dados são válidos.
 Inicio teste 10 -- para passar o 10a
-* Para cadastrar um pokemon, usar a url 
-{site_treinador}/treinador/{nome_treinador}/{apelido_pokemon}, enviando um arquivo json 
-com a chave tipo (por exemplo tipo=pikachu) e a chave experiência
+* Para cadastrar um pokemon, usar a url
+{site_treinador}/treinador/{nome_treinador}/{apelido_pokemon}, enviando um
+arquivo json com a chave tipo (por exemplo tipo=pikachu) e a chave experiência
 * Para enviar um dicionario pra uma URL, usando o verbo put, faça o seguinte:
 requests.put(url,json = {"tipo":"pikachu","experiencia"...})
 
 
-Mais dicas teste 10: 
-* Pode ser necessário usar a pokeapi para verificar se um pokemon existe -- se eu falar que o geremias é dono de um pokemon do tipo homer, deve ocorrer uma excessao, porque homer não é uma espécie válida de pokemon
+Mais dicas teste 10:
+* Pode ser necessário usar a pokeapi para verificar se um pokemon existe -- se
+eu falar que o geremias é dono de um pokemon do tipo homer, deve ocorrer uma
+excessao, porque homer não é uma espécie válida de pokemon
 * Se voce receber um status 404, isso indica um treinador nao encontrado
 * Se voce receber um status 409, isso indica que o pokemon já existia e você
 está fazendo um cadastro dobrado
@@ -301,24 +332,28 @@ está fazendo um cadastro dobrado
 """
 
 
-def cadastrar_pokemon(nome_treinador, apelido_pokemon, tipo_pokemon, experiencia):
+def cadastrar_pokemon(nome_treinador, apelido_pokemon, tipo_pokemon,
+                      experiencia):
     pass
 
 
 """
-11. Dado um nome de treinador, um apelido de pokémon e uma quantidade de experiência, localize esse pokémon e acrescente-lhe a experiência ganha.
+11. Dado um nome de treinador, um apelido de pokémon e uma quantidade de
+experiência, localize esse pokémon e acrescente-lhe a experiência ganha.
 Dicas ex 11:
 utilize a URL {site_treinador}/treinador/{nome_treinador}/{apelido_pokemon}/exp
 Por exemplo, se for o pokemon com apelido terra
 do treinador lucas, a URL ficaria: {site_treinador}/treinador/lucas/terra/exp
 
 
-Utilize o verbo POST, enviando um arquivo json com a chave experiencia (o valor dessa chave é o tanto de exp que eu quero acrescentar)
+Utilize o verbo POST, enviando um arquivo json com a chave experiencia
+(o valor dessa chave é o tanto de exp que eu quero acrescentar)
 
 Para enviar um request com o verbo post, use requests.post(url,...)
 
-Um cod de status 404 pode significar 2 coisas distintas: ou o treinador não existe,
-ou o treinador existe mas o pokemon não. Isso pode verificado acessando a resposta.text
+Um cod de status 404 pode significar 2 coisas distintas: ou o treinador não
+existe, ou o treinador existe mas o pokemon não. Isso pode verificado acessando
+a resposta.text
 (em vez do usual, que seria resposta.json())
 
 O cod de status de sucesso é o 204
@@ -346,26 +381,34 @@ class Pokemon:
 
 
 """
-12. Dado um nome de treinador e um apelido de pokémon, localize esse pokémon na API do treinador e retorne um objeto da classe Pokemon, prenchida com os atributos definidos na classe
+12. Dado um nome de treinador e um apelido de pokémon, localize esse pokémon
+na API do treinador e retorne um objeto da classe Pokemon, prenchida com os
+atributos definidos na classe.
 Dicas 12:
-pegar os dados na url "{site_treinador}/treinador/{nome_treinador}/{apelido_pokemon}"
+pegar os dados na url "{site_treinador}/treinador/{nome_treinador}/
+{apelido_pokemon}"
 acessada com o verbo GET
 para preencher o objeto Pokemon, voce vai fornecer
 * nome treinador (veio como argumento da funcao)
 * apelido pokemon (veio como argumento da funcao)
 * tipo (veio do get que você fez -- chave tipo do dicionário)
 * experiencia (veio do request que você fez -- chave experiencia do dicionário)
-* nivel do pokemon (calcular usando a pokeapi -- voce ja fez essa funcao, use ela)
-* cor do pokemon (em portugues, pegar da pokeapi -- voce ja fez essa funcao, use ela)
+* nivel do pokemon (calcular usando a pokeapi -- voce ja fez essa funcao, use
+ela)
+* cor do pokemon (em portugues, pegar da pokeapi -- voce ja fez essa funcao,
+use ela)
 * evolucao anterior (pegar da pokeapi -- voce ja fez essa funcao, use ela)
 Retornar o objeto pokemon
-Erros 404 podem ser treinador nao existe ou pokemon nao existe -- verifique resposta.text para ver qual dos dois -- já fizemos isso antes
+Erros 404 podem ser treinador nao existe ou pokemon nao existe -- verifique
+resposta.text para ver qual dos dois -- já fizemos isso antes
 
 para criar o objeto do tipo pokemon, já temos uma classe
 
 Podemos construir um objeto do tipo pokemon assim:
 
-Pokemon(nome_treinador, apelido_pokemon, tipo, experiencia, nivel_do_pokemon(tipo, experiencia), cor_do_pokemon(tipo), evolucao_anterior(tipo))
+Pokemon(nome_treinador, apelido_pokemon, tipo, experiencia,
+nivel_do_pokemon(tipo, experiencia), cor_do_pokemon(tipo),
+evolucao_anterior(tipo))
 """
 
 
@@ -374,7 +417,9 @@ def localizar_pokemon(nome_treinador, apelido_pokemon):
 
 
 """
-13. Dado o nome de um treinador, localize-o na API do treinador e retorne um dicionário dos seus pokemons. As chaves do dicionário serão os apelidos dos pokémons dele, e os valores serão os tipos (pikachu, bulbasaur ...) deles.
+13. Dado o nome de um treinador, localize-o na API do treinador e retorne um
+dicionário dos seus pokemons. As chaves do dicionário serão os apelidos dos
+pokémons dele, e os valores serão os tipos (pikachu, bulbasaur ...) deles.
 
 Essas informações estão na URL "{site_treinador}/treinador/{nome_treinador}",
 acessiveis com o verbo GET
@@ -388,10 +433,13 @@ def detalhar_treinador(nome_treinador):
 
 
 """
-14. Dado o nome de um treinador, localize-o na API do treinador e exclua-o, juntamente com todos os seus pokémons.
+14. Dado o nome de um treinador, localize-o na API do treinador e exclua-o,
+juntamente com todos os seus pokémons.
 
-Usar o verbo delete na url do treinador. A mesma que a gente já usou várias vezes.
-O status code vai de informar se o treinador não existia (com qual status code?)
+Usar o verbo delete na url do treinador. A mesma que a gente já usou várias
+vezes.
+O status code vai de informar se o treinador não existia
+(com qual status code?)
 
 Para enviar um request com o verbo delete, use requests.delete(url)
 """
@@ -402,11 +450,14 @@ def excluir_treinador(nome_treinador):
 
 
 """
-15. Dado o nome de um treinador e o apelido de um de seus pokémons, localize o pokémon na API do treinador e exclua-o.
+15. Dado o nome de um treinador e o apelido de um de seus pokémons, localize o
+pokémon na API do treinador e exclua-o.
 
-Usar o verbo delete na url do pokemon: {site_treinador}/treinador/{nome_treinador}/{apelido_pokemon}
-O status code vai de informar se o treinador não existe, ou se o pokemon nao existe 
-(status code 404, não deixe de verificar se foi o pokemon ou treinador que não existia)
+Usar o verbo delete na url do pokemon: {site_treinador}/treinador/
+{nome_treinador}/{apelido_pokemon}
+O status code vai de informar se o treinador não existe, ou se o pokemon nao
+existe (status code 404, não deixe de verificar se foi o pokemon ou treinador
+que não existia)
 """
 
 

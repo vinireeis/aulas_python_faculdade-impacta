8926f322284cf72fced942dede4ba702c4581174
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-09-30 11:45:00 -0300

    correção pep8

--- Aula06/Aula06.py
+++ Aula06/Aula06.py
@@ -1,14 +1,15 @@
 class Pessoa:
-    #contrutor
+    # contrutor
     def __init__(self, nome, email, telefone):
-        #atributos
+        # atributos
         self.nome = nome
         self.email = email
         self.telefone = telefone
 
-#testando objetos
+
+# testando objetos
 pessoa1 = Pessoa("Vinicius", "vih-reis@hotmail.com", "11952945737")
 
-print ("Nome:", pessoa1.nome)
-print ("email:", pessoa1.email)
-print ("Telefone:", pessoa1.telefone)
+print("Nome:", pessoa1.nome)
+print("email:", pessoa1.email)
+print("Telefone:", pessoa1.telefone)
